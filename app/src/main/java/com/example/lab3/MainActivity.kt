d07f6943eedf2b9d4e844a06ad0c63a365a688fc
MYF <1034934778@qq.com>
2023-04-24 19:04:35 +0200

    activity jump

--- app/src/main/java/com/example/lab3/MainActivity.kt
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.lab3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.lab3.database.AppDatabase
 import com.example.lab3.database.entity.User
 
@@ -12,11 +14,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        db = AppDatabase.getDatabase(application)
-//        db.userDao().getAll()
-        val u = User(1,"abc","bcd","1234")
+//        db = AppDatabase.getDatabase(application)
+//        val u = User(1,"abc","bcd","1234")
 //        db.userDao().save(u)
-        db.userDao().getUserById(1)
+//        val a = db.userDao().getUserById(1)
+//        val a = db.userDao().getAll()
+//        println(">>>>>>>>>> ${a.value}")
+
+        val b1 = findViewById<Button>(R.id.button1)
+        val b2 = findViewById<Button>(R.id.button2)
+
+        b1.setOnClickListener{
+            val intent = Intent(this, MyReservationActivity::class.java)
+            startActivity(intent)
+        }
+        b2.setOnClickListener{
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
