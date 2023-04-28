5155a87fc2fe3c1e25573537eb57f1256c164f1f
MYF <1034934778@qq.com>
2023-04-28 22:55:40 +0200

    month text

--- app/src/main/java/com/example/lab3/Calender.kt
+++ app/src/main/java/com/example/lab3/Calender.kt
@@ -1,25 +1,21 @@
 package com.example.lab3
 
-import android.content.Context
+import android.graphics.Color
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.util.TypedValue
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.view.children
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.lab3.databinding.CalendarDayTitlesContainerBinding
 import com.example.lab3.databinding.CalenderDayLayoutBinding
 import com.example.lab3.databinding.FragmentCalenderViewBinding
 import com.kizitonwose.calendar.core.CalendarDay
 import com.kizitonwose.calendar.core.CalendarMonth
+import com.kizitonwose.calendar.core.DayPosition
 import com.kizitonwose.calendar.core.daysOfWeek
-import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import com.kizitonwose.calendar.core.nextMonth
+import com.kizitonwose.calendar.core.previousMonth
 import com.kizitonwose.calendar.view.MonthDayBinder
 import com.kizitonwose.calendar.view.MonthHeaderFooterBinder
 import com.kizitonwose.calendar.view.ViewContainer
@@ -43,44 +39,10 @@ class Calender : Fragment(R.layout.fragment_calender_view) {
 //    ): View? {
 //        return inflater.inflate(R.layout.fragment_calender_view, container, false)
 //    }
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        println("onAttach")
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        println("onCreate")
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        println("onCreateView")
-        return super.onCreateView(inflater, container, savedInstanceState)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        println("onDestroyView")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        println("onDestroy")
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        println("onDetach")
-
-    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentCalenderViewBinding.bind(view)
-        println("onViewCreated")
+
 
         val currentMonth = YearMonth.now() // 2023-04
         val startMonth = currentMonth.minusMonths(100) //2014-12
@@ -91,14 +53,21 @@ class Calender : Fragment(R.layout.fragment_calender_view) {
         binding.calendarView.setup(startMonth, endMonth, daysOfWeek.first())
         binding.calendarView.scrollToMonth(currentMonth)
 
-//        val titlesContainer = view.findViewById<ViewGroup>(R.id.titlesContainer)
-//        titlesContainer.children
-//            .map { it as TextView }
-//            .forEachIndexed { index, textView ->
-//                val dayOfWeek = daysOfWeek[index]
-//                val title = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
-//                textView.text = title
-//            }
+        binding.calendarView.monthScrollListener = { month->
+            binding.monthYearText.text = month.yearMonth.displayText()
+
+        }
+
+        binding.nextMonthImage.setOnClickListener {
+            binding.calendarView.findFirstVisibleMonth()?.let {
+                binding.calendarView.smoothScrollToMonth(it.yearMonth.nextMonth)
+            }
+        }
+        binding.previousMonthImage.setOnClickListener {
+            binding.calendarView.findFirstVisibleMonth()?.let {
+                binding.calendarView.smoothScrollToMonth(it.yearMonth.previousMonth)
+            }
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -109,12 +78,15 @@ class Calender : Fragment(R.layout.fragment_calender_view) {
 
     private fun configureBinders(daysOfWeek: List<DayOfWeek>) {
         class DayViewContainer(view: View): ViewContainer(view) {
+            lateinit var day :CalendarDay
             val binding =CalenderDayLayoutBinding.bind(view)
-            val context = binding.root.context
-            val textView = binding.dayText
-            val layout = binding.dayLayout
-//            val textView = view.findViewById<TextView>(R.id.exFiveDayText)
 
+//            val textView = view.findViewById<TextView>(R.id.exFiveDayText)
+            init {
+                view.setOnClickListener{
+                    println("Click!")
+                }
+            }
         }
         binding.calendarView.dayBinder = object : MonthDayBinder<DayViewContainer>{
             // called only when a new container is needed
@@ -122,7 +94,19 @@ class Calender : Fragment(R.layout.fragment_calender_view) {
 
             //called every time we need to reuse a container  显示日期
             override fun bind(container: DayViewContainer, data: CalendarDay) {
-                container.textView.text = data.date.dayOfMonth.toString()
+                container.day = data
+                val context = container.binding.root.context
+                val textView = container.binding.dayText
+                val layout = container.binding.dayLayout
+                textView.text = data.date.dayOfMonth.toString()
+                if (data.position == DayPosition.MonthDate) {
+                    textView.setTextColor(Color.WHITE)
+                    textView.setTextSize(18F)
+                } else {
+                    textView.setTextColor(Color.GRAY)
+                    textView.setTextSize(15F)
+
+                }
             }
 
         }
@@ -130,7 +114,7 @@ class Calender : Fragment(R.layout.fragment_calender_view) {
 //            val legendLayout = CalendarDayTitlesContainerBinding.bind(view).root
             val titlesContainer = view.findViewById<ViewGroup>(R.id.titlesContainer)
         }
-
+        // header: Mon|Tue|Wed|Thu|Fri|Sat|Sun
         binding.calendarView.monthHeaderBinder =
             object : MonthHeaderFooterBinder<MonthViewContainer>{
 
@@ -145,6 +129,8 @@ class Calender : Fragment(R.layout.fragment_calender_view) {
                                 val dayOfWeek = daysOfWeek[index]
                                 val title = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
                                 tv.text = title
+                                tv.setTextColorRes(R.color.white)
+
                             }
                     }
                 }
