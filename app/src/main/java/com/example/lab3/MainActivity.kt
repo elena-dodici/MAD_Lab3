68373b13fb453b4c63d17eb71b374d9ffae55e20
elena-dodici <s229114@studenti.polito.it>
2023-05-05 16:30:37 +0100

    navBar

--- app/src/main/java/com/example/lab3/MainActivity.kt
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -4,14 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.lab3.database.AppDatabase
 import com.example.lab3.database.entity.User
 import com.example.lab3.databinding.ActivityMainBinding
 import com.example.lab3.databinding.ActivityMyReservationBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     internal lateinit var binding: ActivityMainBinding
-
+    private lateinit var navController: NavController
     lateinit var db:AppDatabase
 //    lateinit var adapter:MainAdapter
 
@@ -19,6 +23,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottonNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavView)
+        bottonNavigationView.uncheckAllItems()
+        setupWithNavController(bottonNavigationView, navController)
         setSupportActionBar(binding.activityToolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
 
@@ -29,29 +39,36 @@ class MainActivity : AppCompatActivity() {
 //        val a = db.userDao().getAll()
 //        println(">>>>>>>>>> ${a.value}")
 
-        val b1 = findViewById<Button>(R.id.button1)
-        val b2 = findViewById<Button>(R.id.button2)
-
-        b1.setOnClickListener{
-//            val intent = Intent(this, MyReservationActivity::class.java)
-//            startActivity(intent)
-            if(savedInstanceState == null){
-                supportFragmentManager.beginTransaction()
-                    .add(R.id.fragmentContainerView,Calendar(),Calendar.javaClass.simpleName)
-//                .replace(R.id.fragmentContainerView,Calender(),Calender.javaClass.simpleName )
-                    .addToBackStack(Calendar.javaClass.simpleName)
-                    .commit()
-            }
+//        val b1 = findViewById<Button>(R.id.button1)
+//        val b2 = findViewById<Button>(R.id.button2)
+//
+//        b1.setOnClickListener{
+////            val intent = Intent(this, MyReservationActivity::class.java)
+////            startActivity(intent)
+//            if(savedInstanceState == null){
+//                supportFragmentManager.beginTransaction()
+//                    .add(R.id.fragmentContainerView,Calendar(),Calendar.javaClass.simpleName)
+////                .replace(R.id.fragmentContainerView,Calender(),Calender.javaClass.simpleName )
+//                    .addToBackStack(Calendar.javaClass.simpleName)
+//                    .commit()
+//            }
+//        }
+//        b2.setOnClickListener{
+//            if(savedInstanceState == null){
+//                supportFragmentManager.beginTransaction()
+//                    .add(R.id.fragmentContainerView,SearchFragment(),SearchFragment.javaClass.simpleName)
+////                .replace(R.id.fragmentContainerView,Calender(),Calender.javaClass.simpleName )
+//                    .addToBackStack(Calendar.javaClass.simpleName)
+//                    .commit()
+//            }
+//        }
+    }
+    fun BottomNavigationView.uncheckAllItems() {
+        menu.setGroupCheckable(0, true, false)
+        for (i in 0 until menu.size()) {
+            menu.getItem(i).isChecked = false
         }
-        b2.setOnClickListener{
-            if(savedInstanceState == null){
-                supportFragmentManager.beginTransaction()
-                    .add(R.id.fragmentContainerView,SearchFragment(),SearchFragment.javaClass.simpleName)
-//                .replace(R.id.fragmentContainerView,Calender(),Calender.javaClass.simpleName )
-                    .addToBackStack(Calendar.javaClass.simpleName)
-                    .commit()
-            }
-        }
+        menu.setGroupCheckable(0, true, true)
     }
 
 }
