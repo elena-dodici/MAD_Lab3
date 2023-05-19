af9f84c427c9a19155faaa0a6c07cb804b7bf454
elena-dodici <s229114@studenti.polito.it>
2023-05-19 21:46:25 +0100

    toolbar

--- app/src/main/java/com/example/lab3/CourtDetailFragment.kt
+++ app/src/main/java/com/example/lab3/CourtDetailFragment.kt
@@ -1,72 +1,3 @@
-//package com.example.lab3
-//
-//import android.os.Bundle
-//import androidx.fragment.app.Fragment
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import android.widget.Toast
-//import androidx.core.view.isVisible
-//import androidx.fragment.app.activityViewModels
-//import androidx.navigation.fragment.findNavController
-//import com.example.lab3.databinding.FragmentDetailBinding
-//
-//
-//
-///**
-// * A simple [Fragment] subclass.
-// * Use the [CourtDetailFragment.newInstance] factory method to
-// * create an instance of this fragment.
-// */
-//class CourtDetailFragment : BaseFragment(R.layout.fragment_court_detail) {
-//
-//    private lateinit var binding: FragmentDetailBinding
-//    private val sharedvm : CourtViewModel by activityViewModels()
-//
-//
-//
-////    override fun onCreateView(
-////        inflater: LayoutInflater, container: ViewGroup?,
-////        savedInstanceState: Bundle?
-////    ): View? {
-////        // Inflate the layout for this fragment
-////        return inflater.inflate(R.layout.fragment_court_detail, container, false)
-////
-////    }
-//
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        binding = FragmentDetailBinding.bind(view, savedInstanceState)
-////        if (sharedvm.hasRev.value!!){
-////            binding.delBtn.visibility = View.VISIBLE;
-////        }
-////        else binding.delBtn.visibility = View.GONE
-//
-//        binding.delBtn.setOnClickListener {
-//            //if delete bottom show, it must means has review
-//           // sharedvm.deleteCourtRev(this.requireActivity().application)
-//            findNavController().navigate(R.id.action_courtDetailFragment_to_courtFragment)
-//            Toast.makeText(context, "Delete successfully", Toast.LENGTH_LONG).show()
-//        }
-//        binding.saveBtn.setOnClickListener {
-//
-////               sharedvm.addOrUpdateCourtRev(this.requireActivity().application)
-//                findNavController().navigate(R.id.action_courtDetailFragment_to_courtFragment)
-//                Toast.makeText(context, "Update successfully", Toast.LENGTH_LONG).show()
-//
-//
-//        }
-//        binding.calBtn.setOnClickListener {
-//            findNavController().navigate(R.id.action_courtDetailFragment_to_courtFragment)
-//            Toast.makeText(context, "Your Review is not saved!", Toast.LENGTH_LONG).show()
-//        }
-//
-//    }
-//
-//
-//
-//}
 
 package com.example.lab3
 
@@ -77,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -89,11 +21,12 @@ import com.example.lab3.databinding.FragmentCourtDetailBinding
  * Use the [CourtDetailFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CourtDetailFragment : BaseFragment(R.layout.fragment_court_detail) {
+class CourtDetailFragment : BaseFragment(R.layout.fragment_court_detail), HasToolbar  {
 
     private lateinit var binding: FragmentCourtDetailBinding
     private val sharedvm: CourtViewModel by activityViewModels()
-
+    override val toolbar: Toolbar?
+        get() = binding.activityToolbar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
