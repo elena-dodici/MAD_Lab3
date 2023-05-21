cf1a22ea34ce8cf9df24beedbc8206cc4c810b0b
elena-dodici <s229114@studenti.polito.it>
2023-05-21 14:52:29 +0100

    INITDB

--- app/src/main/java/com/example/lab3/MainActivity.kt
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -52,7 +52,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
-//     initDatabase(db) // add some initial data
+     initDatabase(db) // pre_saved the data in our DB
 
 
     }
@@ -87,97 +87,79 @@ class MainActivity : AppCompatActivity() {
         )
         @Suppress("DEPRECATION")
         val ct = listOf<CourtTime>(
-            CourtTime(1, Time(9,0,0),Time(10,0,0)),
+
             CourtTime(1, Time(10,0,0),Time(11,0,0)),
             CourtTime(1, Time(11,0,0),Time(12,0,0)),
-            CourtTime(1, Time(12,0,0),Time(13,0,0)),
-            CourtTime(1, Time(13,0,0),Time(14,0,0)),
+
             CourtTime(1, Time(14,0,0),Time(15,0,0)),
             CourtTime(1, Time(15,0,0),Time(16,0,0)),
             CourtTime(1, Time(16,0,0),Time(17,0,0)),
-            CourtTime(1, Time(17,0,0),Time(18,0,0)),
-            CourtTime(1, Time(18,0,0),Time(19,0,0)),
 
-            CourtTime(2, Time(9,0,0),Time(10,0,0)),
-            CourtTime(2, Time(10,0,0),Time(11,0,0)),
-            CourtTime(2, Time(11,0,0),Time(12,0,0)),
-            CourtTime(2, Time(12,0,0),Time(13,0,0)),
+
+
             CourtTime(2, Time(13,0,0),Time(14,0,0)),
             CourtTime(2, Time(14,0,0),Time(15,0,0)),
             CourtTime(2, Time(15,0,0),Time(16,0,0)),
             CourtTime(2, Time(16,0,0),Time(17,0,0)),
             CourtTime(2, Time(17,0,0),Time(18,0,0)),
-            CourtTime(2, Time(18,0,0),Time(19,0,0)),
+            CourtTime(2, Time(18,0,0),Time(19,30,0)),
 
             CourtTime(3, Time(9,0,0),Time(10,0,0)),
             CourtTime(3, Time(10,0,0),Time(11,0,0)),
             CourtTime(3, Time(11,0,0),Time(12,0,0)),
             CourtTime(3, Time(12,0,0),Time(13,0,0)),
-            CourtTime(3, Time(13,0,0),Time(14,0,0)),
-            CourtTime(3, Time(14,0,0),Time(15,0,0)),
-            CourtTime(3, Time(15,0,0),Time(16,0,0)),
-            CourtTime(3, Time(16,0,0),Time(17,0,0)),
-            CourtTime(3, Time(17,0,0),Time(18,0,0)),
-            CourtTime(3, Time(18,0,0),Time(19,0,0)),
+
 
             CourtTime(4, Time(9,0,0),Time(10,0,0)),
             CourtTime(4, Time(10,0,0),Time(11,0,0)),
             CourtTime(4, Time(11,0,0),Time(12,0,0)),
-            CourtTime(4, Time(12,0,0),Time(13,0,0)),
-            CourtTime(4, Time(13,0,0),Time(14,0,0)),
-            CourtTime(4, Time(14,0,0),Time(15,0,0)),
-            CourtTime(4, Time(15,0,0),Time(16,0,0)),
-            CourtTime(4, Time(16,0,0),Time(17,0,0)),
-            CourtTime(4, Time(17,0,0),Time(18,0,0)),
-            CourtTime(4, Time(18,0,0),Time(19,0,0)),
 
-            CourtTime(5, Time(9,0,0),Time(10,0,0)),
-            CourtTime(5, Time(10,0,0),Time(11,0,0)),
-            CourtTime(5, Time(11,0,0),Time(12,0,0)),
-            CourtTime(5, Time(12,0,0),Time(13,0,0)),
-            CourtTime(5, Time(13,0,0),Time(14,0,0)),
-            CourtTime(5, Time(14,0,0),Time(15,0,0)),
+
+
+
+
             CourtTime(5, Time(15,0,0),Time(16,0,0)),
             CourtTime(5, Time(16,0,0),Time(17,0,0)),
             CourtTime(5, Time(17,0,0),Time(18,0,0)),
             CourtTime(5, Time(18,0,0),Time(19,0,0)),
+            CourtTime(5, Time(19,0,0),Time(20,0,0)),
         )
 
-        val reservations = listOf<Reservation>(
-            Reservation(1, 1, 0, LocalDate.of(2023,5,1),"res1"),
-            Reservation(2, 1, 0, LocalDate.of(2023,5,4),"res2"),
-            Reservation(3, 1, 0, LocalDate.of(2023,5,1),"res3"),
-            Reservation(11, 1, 0, LocalDate.of(2023,5,1),"res4"),
-            Reservation(12, 1, 0, LocalDate.of(2023,5,6),"res5"),
-            Reservation(13, 1, 0, LocalDate.of(2023,5,1),"res6"),
-            Reservation(21, 1, 0, LocalDate.of(2023,4,23),"res7"),
-            Reservation(22, 1, 0, LocalDate.of(2023,5,1),"res8"),
-            Reservation(23, 1, 0, LocalDate.of(2023,5,4),"res9"),
-
-            Reservation(11, 1, 0, LocalDate.of(2023,5,3),"res9"),
-            Reservation(12, 1, 0, LocalDate.of(2023,5,3),"res9"),
-            Reservation(13, 1, 0, LocalDate.of(2023,5,3),"res9"),
-            Reservation(14, 1, 0, LocalDate.of(2023,5,3),"res9"),
-            Reservation(15, 1, 0, LocalDate.of(2023,5,3),"res9"),
-            Reservation(16, 1, 0, LocalDate.of(2023,5,3),"res9"),
-            Reservation(17, 1, 0, LocalDate.of(2023,5,3),"res9"),
-            Reservation(18, 1, 0, LocalDate.of(2023,5,3),"res9"),
-            Reservation(19, 1, 0, LocalDate.of(2023,5,3),"res9"),
-            Reservation(20, 1, 0, LocalDate.of(2023,5,3),"res9"),
-
-
-            )
-        val courtReviews = listOf<CourtReview>(
-            CourtReview(1,1,4,"I had a great experience here. The staff was professional and helpful, guiding me through the legal process with patience and clarity. The courtrooms were well-maintained, and the proceedings were conducted fairly and efficiently"),
-            CourtReview(1,2,3,"The court demonstrated a high level of professionalism and integrity. The judges were knowledgeable and unbiased, ensuring a fair and just outcome. The support staff was friendly and accommodating, making the entire process less intimidating."),
-            CourtReview(1,3,5,"I had a positive experience for here. The judges were highly knowledgeable and demonstrated a deep understanding of the law. The court maintained a respectful and dignified atmosphere, fostering a sense of trust and credibility. Overall, the court provided a fair and transparent judicial process"),
-            CourtReview(2,1,3,"The proceedings were conducted in a timely manner, and the judges paid close attention to details. The court facilities were modern and comfortable, creating a conducive environment for justice to be served"),
-            CourtReview(2,3,3,"It exceeded my expectations. The judges were not only knowledgeable but also empathetic, considering all aspects of the case before making a decision. The courtrooms were well-equipped, and the entire process was conducted with utmost professionalism"),
-            CourtReview(2,4,3,"This is highly organized and transparent. The judges were diligent in explaining legal complexities in a way that was understandable to all parties involved. The court's commitment to fairness and adherence to due process were evident throughout the proceedings"),
-            CourtReview(3,1,4," I appreciated the court's commitment to justice."),
+//        val reservations = listOf<Reservation>(
+//            Reservation(1, 1, 0, LocalDate.of(2023,5,1),"res1"),
+//            Reservation(2, 1, 0, LocalDate.of(2023,5,4),"res2"),
+//            Reservation(3, 1, 0, LocalDate.of(2023,5,1),"res3"),
+//            Reservation(11, 1, 0, LocalDate.of(2023,5,1),"res4"),
+//            Reservation(12, 1, 0, LocalDate.of(2023,5,6),"res5"),
+//            Reservation(13, 1, 0, LocalDate.of(2023,5,1),"res6"),
+//            Reservation(21, 1, 0, LocalDate.of(2023,4,23),"res7"),
+//            Reservation(22, 1, 0, LocalDate.of(2023,5,1),"res8"),
+//            Reservation(23, 1, 0, LocalDate.of(2023,5,4),"res9"),
+//
+//            Reservation(11, 1, 0, LocalDate.of(2023,5,3),"res9"),
+//            Reservation(12, 1, 0, LocalDate.of(2023,5,3),"res9"),
+//            Reservation(13, 1, 0, LocalDate.of(2023,5,3),"res9"),
+//            Reservation(14, 1, 0, LocalDate.of(2023,5,3),"res9"),
+//            Reservation(15, 1, 0, LocalDate.of(2023,5,3),"res9"),
+//            Reservation(16, 1, 0, LocalDate.of(2023,5,3),"res9"),
+//            Reservation(17, 1, 0, LocalDate.of(2023,5,3),"res9"),
+//            Reservation(18, 1, 0, LocalDate.of(2023,5,3),"res9"),
+//            Reservation(19, 1, 0, LocalDate.of(2023,5,3),"res9"),
+//            Reservation(20, 1, 0, LocalDate.of(2023,5,3),"res9"),
+//
+//
+//            )
+//        val courtReviews = listOf<CourtReview>(
+//            CourtReview(1,1,4,"I had a great experience here. The staff was professional and helpful, guiding me through the legal process with patience and clarity. The courtrooms were well-maintained, and the proceedings were conducted fairly and efficiently"),
+//            CourtReview(1,2,3,"The court demonstrated a high level of professionalism and integrity. The judges were knowledgeable and unbiased, ensuring a fair and just outcome. The support staff was friendly and accommodating, making the entire process less intimidating."),
+//            CourtReview(1,3,5,"I had a positive experience for here. The judges were highly knowledgeable and demonstrated a deep understanding of the law. The court maintained a respectful and dignified atmosphere, fostering a sense of trust and credibility. Overall, the court provided a fair and transparent judicial process"),
+//            CourtReview(2,1,3,"The proceedings were conducted in a timely manner, and the judges paid close attention to details. The court facilities were modern and comfortable, creating a conducive environment for justice to be served"),
+//            CourtReview(2,3,3,"It exceeded my expectations. The judges were not only knowledgeable but also empathetic, considering all aspects of the case before making a decision. The courtrooms were well-equipped, and the entire process was conducted with utmost professionalism"),
+//            CourtReview(2,4,3,"This is highly organized and transparent. The judges were diligent in explaining legal complexities in a way that was understandable to all parties involved. The court's commitment to fairness and adherence to due process were evident throughout the proceedings"),
+//            CourtReview(3,1,4," I appreciated the court's commitment to justice."),
 //            CourtReview(3,5,1,"faulty net and neglected place overall"),
 
-        )
+  //      )
 //        val sportDetails = listOf<SportDetail>(
 //            SportDetail(1,"running",3,"achievement") ,
 //            SportDetail(1,"basketball",2,"achievement"),
@@ -202,12 +184,12 @@ class MainActivity : AppCompatActivity() {
         for (c in ct){
             db.courtTimeDao().addCourtTime(c)
         }
-        for (r in reservations){
-            db.reservationDao().addReservation(r)
-        }
-        for(cr in courtReviews){
-            db.courtReviewDao().addCourtReview(cr)
-        }
+//        for (r in reservations){
+//            db.reservationDao().addReservation(r)
+//        }
+//        for(cr in courtReviews){
+//            db.courtReviewDao().addCourtReview(cr)
+//        }
 //        for(s in sportDetails){
 //            db.sportDetailDao().addSportDetails(s)
 //        }
