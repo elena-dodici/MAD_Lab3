f18ba64665bca1b43d86c4e5db53d7453b63f9a0
MYF <1034934778@qq.com>
2023-05-30 23:28:44 +0200

    solve dependence problems etc. we can use Compose now

---
+++ app/src/main/java/com/example/lab3/MyFragment.kt
@@ -0,0 +1,30 @@
+package com.example.lab3
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+class MyFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val composeView = ComposeView(requireContext())
+        composeView.setLayoutParams(
+            ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+        )
+        composeView.setContent {
+            MyComposeView()
+        }
+
+        return composeView
+    }
+}
