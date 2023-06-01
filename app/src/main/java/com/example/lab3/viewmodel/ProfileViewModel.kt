11e58f2080ed2426d2148c6433468f10cb54d2d3
uniqueCapricorn <736076274@qq.com>
2023-06-01 22:08:48 +0200

    upload photo and save it in firebase

--- app/src/main/java/com/example/lab3/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/example/lab3/viewmodel/ProfileViewModel.kt
@@ -1,22 +1,26 @@
 package com.example.lab3
 
 import android.app.Application
+import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.room.ColumnInfo
 import com.example.lab3.database.AppDatabase
 import com.example.lab3.database.entity.CourtInfo
 import com.example.lab3.database.entity.SportDetail
 import com.example.lab3.database.entity.User
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 class ProfileViewModel : ViewModel() {
 
     lateinit var db: AppDatabase
     val db1 = Firebase.firestore
-    private var _User = MutableLiveData<User>()
-    val User:LiveData<User> = _User
+    val storage = Firebase.storage
+    private var _User = MutableLiveData<UserProfile>()
+    val User:LiveData<UserProfile> = _User
     private var _userSports = MutableLiveData<List<SportDetail>>().also { it.value = listOf() }
     val userSports:LiveData<List<SportDetail>> = _userSports
 
@@ -24,24 +28,64 @@ class ProfileViewModel : ViewModel() {
 //        db = AppDatabase.getDatabase(application)
 //        _User.value = db.userDao().getUserById(id)
 //    }
+public data class UserProfile(
+    var name:String,
+    var surname:String,
+    var tel:String?,
+    var photo:String?
+)
     fun getUserById(application: Application, id:Int){
 
     db1.collection("users").document("u${id}").get()
         .addOnSuccessListener { result ->
-            _User.value = User(result["name"].toString(),result["surname"].toString(),result["tel"].toString())
+            _User.value = UserProfile(result["name"].toString(),result["surname"].toString(),result["tel"].toString(),result["photo"].toString())
         }
         .addOnFailureListener { exception ->
             println("Error getting documents: ${exception.message}")
-            _User.value = User("","","")
+            _User.value = UserProfile("","","","")
         }
 
 }
+    fun uploadPhoto(application: Application, photo: Uri,id:Int){
+        val storageRef = storage.reference
+        val storagePath = "user${id}/images/" // 存储路径，可以根据自己的需要进行调整
+        val fileName = "user${id}.jpg" // 图片文件名，可以根据自己的需要进行调整
+        val fileRef = storageRef.child("$storagePath$fileName")
+        val uploadTask = fileRef.putFile(photo)
+        uploadTask.addOnSuccessListener { taskSnapshot ->
+            // 图片文件上传成功
+            println("upload success")
+        }.addOnFailureListener { exception ->
+            // 处理上传失败的情况
+        }.addOnProgressListener { taskSnapshot ->
+            // 监听上传进度
+            val progress = (100.0 * taskSnapshot.bytesTransferred / taskSnapshot.totalByteCount).toInt()
+            // 在此处可以更新上传进度条或显示上传进度信息
+        }
+    }
 
-    fun updateUser(application: Application, user: User,id: Int){
+    private var _Uri = MutableLiveData<String>()
+    val photoUri:LiveData<String> = _Uri
+    fun readPhoto(application: Application,path:String){
+        val storageRef = Firebase.storage.reference
+        val imageRef = storageRef.child(path)
+        imageRef.downloadUrl.addOnSuccessListener { uri ->
+
+            _Uri.value = uri.toString()
+            println(_Uri.value)
+        }.addOnFailureListener { exception ->
+            // 处理下载失败的情况
+            println("Error downloading image: ${exception.message}")
+        }
+
+    }
+
+    fun updateUser(application: Application, user: UserProfile,id: Int){
         val data = hashMapOf(
             "name" to user.name,
             "surname" to user.surname,
-            "tel" to user.tel
+            "tel" to user.tel,
+            "photo" to user.photo
         )
         val newD=db1.collection("users").document("u${id}")
         newD.update(data as Map<String, Any>)
@@ -50,6 +94,9 @@ class ProfileViewModel : ViewModel() {
                 println("Error getting documents: ${exception.message}")
             }
     }
+
+
+
 
 //    fun getUserSportsById(application: Application, id:Int){
 //        db = AppDatabase.getDatabase(application)
