d7bc04ae08b9356548876b0ee062c4c5fccb7633
elena-dodici <s229114@studenti.polito.it>
2023-06-05 21:48:02 +0200

    login+page

--- app/src/main/java/com/example/lab3/MainFragment.kt
+++ app/src/main/java/com/example/lab3/MainFragment.kt
@@ -2,7 +2,9 @@ package com.example.lab3
 
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.widget.Toolbar
+import androidx.core.widget.doOnTextChanged
 import com.example.lab3.databinding.FragmentMainBinding
 
 
@@ -22,6 +24,24 @@ class MainFragment : BaseFragment(R.layout.fragment_main), HasToolbar {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMainBinding.bind(view)
+
+        val email = view.findViewById<EditText>(R.id.loginemail)
+        val pw  = view.findViewById<EditText>(R.id.loginpw)
+//        val inputEmail = binding.loginemail?.text.toString()
+//        val inputPassword = binding.loginpw?.text.toString()
+        binding.loginemail?.doOnTextChanged { inputText, _, _, _ ->
+            // Respond to input text change
+            println("I got inputTextl!: ${inputText}")
+        }
+
+        binding.btLogin.setOnClickListener {
+            println("I got inputEmail!: ${email.text}")
+            println("I got input password!: ${pw.text}")
+        }
+
+        binding.register.setOnClickListener {
+            println("i click register|! need to jump to registration page!")
+        }
 
 
     }
